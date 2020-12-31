52b70ec9c0003fc41cd668737a06ebfab5a81479
Brian Daniels <brian.daniels@treetowntech.com>
2020-12-31 14:47:04 -0500

    Initial commit

---
+++ dfplayer.c
@@ -0,0 +1,70 @@
+#include <assert.h>
+
+#include "dfplayer.h"
+
+typedef enum {
+    DFPLAYER_CMD_NEXT_TRACK = 1,
+    DFPLAYER_CMD_PREV_TRACK,
+    DFPLAYER_CMD_SEL_TRACK,
+    DFPLAYER_CMD_INC_VOL,
+    DFPLAYER_CMD_DEC_VOL,
+    DFPLAYER_CMD_SET_VOL,
+    DFPLAYER_CMD_SET_EQ,
+    DFPLAYER_CMD_SET_MODE,
+    DFPLAYER_CMD_SET_SRC,
+    DFPLAYER_CMD_SUSPEND,
+    DFPLAYER_CMD_RESUME,
+    DFPLAYER_CMD_RESET,
+    DFPLAYER_CMD_PLAY,
+    DFPLAYER_CMD_PAUSE,
+    DFPLAYER_CMD_SET_FOLDER,
+    DFPLAYER_CMD_ENABLE_LOOP,
+    DFPLAYER_CMD_DISBALE_LOOP,
+} dfplayer_cmd_t;
+
+static bool dfplayer_send_command(
+    dfplayer_t *self,
+    dfplayer_cmd_t command,
+    uint16_t data
+) {
+    uint8_t buffer[8];
+    buffer[0] = 0x7E;
+    buffer[1] = 0xFF;
+    buffer[2] = 0x06;
+    buffer[3] = command & 0xFF;
+    buffer[4] = 0x00;
+    buffer[5] = data >> 8;
+    buffer[6] = data & 0xFF;
+    buffer[7] = 0xEF;
+
+    assert(self->send_bytes);
+    return self->send_bytes(buffer, 8);
+}
+
+bool dfplayer_reset(dfplayer_t *self) {
+    return dfplayer_send_command(self, DFPLAYER_CMD_RESET, 0);
+}
+
+bool dfplayer_set_eq(dfplayer_t *self, dfplayer_eq_t eq) {
+    return dfplayer_send_command(self, DFPLAYER_CMD_SET_EQ, (uint16_t) (eq & 0xFFFF));
+}
+
+bool dfplayer_set_src(dfplayer_t *self, dfplayer_src_t src) {
+    return dfplayer_send_command(self, DFPLAYER_CMD_SET_SRC, (uint16_t) (src & 0xFFFF));
+}
+
+bool dfplayer_set_mode(dfplayer_t *self, dfplayer_mode_t mode) {
+    return dfplayer_send_command(self, DFPLAYER_CMD_SET_MODE, (uint16_t) (mode & 0xFFFF));
+}
+
+bool dfplayer_set_volume(dfplayer_t *self, uint16_t volume) {
+    if (volume > 30) {
+        volume = 30;
+    }
+
+    return dfplayer_send_command(self, DFPLAYER_CMD_SET_MODE, volume);
+}
+
+bool dfplayer_select_track(dfplayer_t *self, uint16_t track) {
+    return dfplayer_send_command(self, DFPLAYER_CMD_SEL_TRACK, track);
+}
---
+++ dfplayer.h
@@ -0,0 +1,43 @@
+#ifndef INC_DFPLAYER_H_
+#define INC_DFPLAYER_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef enum {
+    DFPLAYER_EQ_NORMAL = 0,
+    DFPLAYER_EQ_POP,
+    DFPLAYER_EQ_ROCK,
+    DFPLAYER_EQ_JAZZ,
+    DFPLAYER_EQ_CLASSIC,
+    DFPLAYER_EQ_BASE, // BASS?
+} dfplayer_eq_t;
+
+typedef enum {
+    DFPLAYER_SRC_USB = 0,
+    DFPLAYER_SRC_SD_CARD,
+    DFPLAYER_SRC_AUX,
+    DFPLAYER_SRC_SLEEP,
+    DFPLAYER_SRC_FLASH,
+} dfplayer_src_t;
+
+typedef enum {
+    DFPLAYER_MODE_SINGLE_PLAY = 0,
+    DFPLAYER_MODE_FOLDER_REPEAT,
+    DFPLAYER_MODE_SINGLE_REPEAT,
+    DFPLAYER_MODE_RANOM,
+} dfplayer_mode_t;
+
+typedef struct {
+    bool (*send_bytes)(uint8_t *, uint32_t);
+} dfplayer_t;
+
+bool dfplayer_reset(dfplayer_t *self);
+bool dfplayer_set_eq(dfplayer_t *self, dfplayer_eq_t eq);
+bool dfplayer_set_src(dfplayer_t *self, dfplayer_src_t src);
+bool dfplayer_set_mode(dfplayer_t *self, dfplayer_mode_t mode);
+bool dfplayer_set_volume(dfplayer_t *self, uint16_t volume);
+bool dfplayer_select_track(dfplayer_t *self, uint16_t track);
+bool dfplayer_reset(dfplayer_t *self);
+
+#endif // INC_DFPLAYER_H_
